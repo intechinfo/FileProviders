04804c86175a0df19edc569f31ce4179956248ba
Anthaax <guillaume.fimes@laposte.net>
2017-06-01 16:59:54 +0200

    Correction for GitFilesProvierDirectoryTest

--- Intech.FileProviders/Tests/Intech.FileProviders.GitFileProvider.Tests/GitFilesProvierDirectoryTest.cs
+++ Intech.FileProviders/Tests/Intech.FileProviders.GitFileProvider.Tests/GitFilesProvierDirectoryTest.cs
@@ -7,7 +7,7 @@ namespace Intech.FileProviders.GitFileProvider.Tests
     [TestFixture]
     public class GitFilesProvierDirectory
     {
-        private string ProjectRootPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+        private string ProjectRootPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.Parent.FullName;
 
         [Test]
         public void Get_directory_with_no_parameters()
@@ -26,65 +26,102 @@ namespace Intech.FileProviders.GitFileProvider.Tests
         public void Get_directory_with_no_parameters_and_path()
         {
             GitFileProvider git = new GitFileProvider(ProjectRootPath);
-            var rootDir = git.GetDirectoryContents("GoM.Core.Abstractions");
+            var rootDir = git.GetDirectoryContents(@"Intech.FileProviders\Intech.FileProviders.GitFileProvider");
 
             rootDir.Exists.Should().BeTrue();
             foreach (var item in rootDir)
             {
                 item.Exists.Should().BeTrue();
-                item.PhysicalPath.Should().Be(@"GoM.Core.Abstractions\" + item.Name);
+                item.PhysicalPath.Should().Be(@"Intech.FileProviders\Intech.FileProviders.GitFileProvider\" + item.Name);
             }
+        }
+        [Test]
+        public void Get_directory_with_no_existing_path()
+        {
+            GitFileProvider git = new GitFileProvider(ProjectRootPath);
+            var rootDir = git.GetDirectoryContents(@"Intech\Intech.FileProviders.GitFileProvider");
+
+            rootDir.Exists.Should().BeFalse();
         }
         [Test]
         public void Get_directory_with_branch_parram()
         {
             GitFileProvider git = new GitFileProvider(ProjectRootPath);
-            var rootDir = git.GetDirectoryContents(@"branches\origin/perso_yazman\GoM.Core.Abstractions");
+            var rootDir = git.GetDirectoryContents(@"branches\dev-Guillaume\Intech.FileProviders\Intech.FileProviders.GitFileProvider");
 
             rootDir.Exists.Should().BeTrue();
             foreach (var item in rootDir)
             {
                 item.Exists.Should().BeTrue();
-                item.PhysicalPath.Should().Be(@"GoM.Core.Abstractions\" + item.Name);
+                item.PhysicalPath.Should().Be(@"Intech.FileProviders\Intech.FileProviders.GitFileProvider\" + item.Name);
             }
+        }
+        [Test]
+        public void Get_directory_with_branch_unexisting_parram()
+        {
+            GitFileProvider git = new GitFileProvider(ProjectRootPath);
+            var rootDir = git.GetDirectoryContents(@"branches\dev-Guillaume\Intech\Intech.FileProviders.GitFileProvider");
+
+            rootDir.Exists.Should().BeFalse();
         }
         [Test]
         public void Get_directory_with_head_parram()
         {
             GitFileProvider git = new GitFileProvider(ProjectRootPath);
-            var headDir = git.GetDirectoryContents(@"head\GoM.GitFileProvider");
+            var headDir = git.GetDirectoryContents(@"head\Intech.FileProviders\Intech.FileProviders.GitFileProvider");
             headDir.Exists.Should().BeTrue();
             foreach (var item in headDir)
             {
                 item.Exists.Should().BeTrue();
-                item.PhysicalPath.Should().Be(@"GoM.GitFileProvider" + Path.DirectorySeparatorChar + item.Name);
+                item.PhysicalPath.Should().Be(@"Intech.FileProviders\Intech.FileProviders.GitFileProvider" + Path.DirectorySeparatorChar + item.Name);
             }
+        }
+        [Test]
+        public void Get_directory_with_head_bad_parram()
+        {
+            GitFileProvider git = new GitFileProvider(ProjectRootPath);
+            var headDir = git.GetDirectoryContents(@"head\Intech\Intech.FileProviders.GitFileProvider");
+            headDir.Exists.Should().BeFalse();
         }
         [Test]
         public void Get_directory_with_commit_parram()
         {
             GitFileProvider git = new GitFileProvider(ProjectRootPath);
-            var rootDir = git.GetDirectoryContents(@"commits\1921471fd36db781bef6833b4723f34afccd8d71\GoM.GitFileProvider");
+            var rootDir = git.GetDirectoryContents(@"commits\9b3bd5db5082c0d4cc41b1a480df897c049ac70b\Intech.FileProviders\Intech.FileProviders.GitFileProvider");
             rootDir.Exists.Should().BeTrue();
             foreach (var item in rootDir)
             {
                 item.Exists.Should().BeTrue();
-                item.PhysicalPath.Should().Be(@"GoM.GitFileProvider\" + item.Name);
+                item.PhysicalPath.Should().Be(@"Intech.FileProviders\Intech.FileProviders.GitFileProvider\" + item.Name);
 
             }
+        }
+        [Test]
+        public void Get_directory_with_commit_bad_parram()
+        {
+            GitFileProvider git = new GitFileProvider(ProjectRootPath);
+            var rootDir = git.GetDirectoryContents(@"commits\9b3bd5db5082c0d4cc41b1a480df897c049ac70b\Intech\Intech.FileProviders.GitFileProvider");
+            rootDir.Exists.Should().BeFalse();
         }
         [Test]
         public void Get_directory_with_tag_parram()
         {
             GitFileProvider git = new GitFileProvider(ProjectRootPath);
-            var rootDir = git.GetDirectoryContents(@"tags\GitWatcher\GoM.GitFileProvider");
+            var rootDir = git.GetDirectoryContents(@"tags\FirstCommit\Intech.FileProviders\Intech.FileProviders.GitFileProvider");
             rootDir.Exists.Should().BeTrue();
             foreach (var item in rootDir)
             {
                 item.Exists.Should().BeTrue();
-                item.PhysicalPath.Should().Be(@"GoM.GitFileProvider\" + item.Name);
+                item.PhysicalPath.Should().Be(@"Intech.FileProviders\Intech.FileProviders.GitFileProvider\" + item.Name);
 
             }
+        }
+        [Test]
+        public void Get_directory_with_tag_bad_parram()
+        {
+            GitFileProvider git = new GitFileProvider(ProjectRootPath);
+            var rootDir = git.GetDirectoryContents(@"tags\FirstCommit\Intech\Intech.FileProviders.GitFileProvider");
+            rootDir.Exists.Should().BeFalse();
         }
         [Test]
         public void Get_directory_with_null_parameters()
@@ -99,8 +136,8 @@ namespace Intech.FileProviders.GitFileProvider.Tests
         public void GlobingPatern()
         {
             GitFileProvider git = new GitFileProvider(ProjectRootPath);
-            var rootDir = git.GetDirectoryContents(@"branches\origin/perso_yazman\*");
-            var file = git.GetFileInfo(@"branches\origin/perso_yazman\*");
+            var rootDir = git.GetDirectoryContents(@"branches\dev-Guillaume\*");
+            var file = git.GetFileInfo(@"branches\dev-Guillaume\*");
 
             rootDir.Exists.Should().BeFalse();
             file.Exists.Should().BeFalse();
@@ -115,7 +152,7 @@ namespace Intech.FileProviders.GitFileProvider.Tests
             rootDir.Exists.Should().BeTrue();
             foreach (var item in rootDir)
             {
-                var branchDir = git.GetDirectoryContents(item.PhysicalPath + @"GoM.Core.Abstractions");
+                var branchDir = git.GetDirectoryContents(item.PhysicalPath + @"Intech.FileProviders.GitFileProvider");
                 foreach (var dir in branchDir)
                 {
                     dir.Exists.Should().BeTrue();
